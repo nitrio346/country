4c054a5c04d1e61079afcc771ef39346aa03108c
Michal5675 <tusk45@interia.pl>
2022-08-29 20:56:14 +0200

    change app.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element ={<Content/>} />
           <Route path= "/:id" element ={<CountryPage/>} />
